f541bd8735bd429366b2a30f9c595de4ed219d64
Shreyansh gohil <p-shraddhamodi@goodearth.in>
2023-05-16 09:37:04 +0530

    Ragister page bug solved

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,47 +25,51 @@ const Home = () => {
       <div className="overflow-x-auto ">
         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
           <div className="overflow-hidden">
-            <table className="min-w-full text-left text-sm font-light">
-              <thead className="border-b font-medium border-neutral-500">
-                <tr>
-                  <th scope="col" className="px-6 py-4">
-                    #
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    UserName
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Email
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Phone No
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((singleUser, index) => {
-                  return (
-                    <tr
-                      className="border-b transition duration-300 ease-in-out  border-neutral-500 hover:bg-neutral-600 hover:text-white"
-                      key={index}
-                    >
-                      <td className="whitespace-nowrap px-6 py-4 font-medium">
-                        {index}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {singleUser.userName}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {singleUser.email}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {singleUser.contactNo}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            {users.length > 0 ? (
+              <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium border-neutral-500">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">
+                      #
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      UserName
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Email
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Phone No
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((singleUser, index) => {
+                    return (
+                      <tr
+                        className="border-b transition duration-300 ease-in-out  border-neutral-500 hover:bg-neutral-600 hover:text-white"
+                        key={index}
+                      >
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">
+                          {index}
+                        </td>
+                        <td className="whitespace-nowrap px-6 py-4">
+                          {singleUser.userName}
+                        </td>
+                        <td className="whitespace-nowrap px-6 py-4">
+                          {singleUser.email}
+                        </td>
+                        <td className="whitespace-nowrap px-6 py-4">
+                          {singleUser.contactNo}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            ) : (
+              <p>No Users Available</p>
+            )}
           </div>
         </div>
       </div>
